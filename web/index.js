d6e1d9a9ecd2e8e4167c3aebec3a596b3e781ee2
Sam <sam.thompson@biarri.com>
2020-07-27 15:26:30 +1000

    stash code for batch geoference of tiles

--- web/index.js
+++ web/index.js
@@ -1,17 +1,16 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {applyTransform} from 'ol/extent';
+import {applyTransform, getWidth} from 'ol/extent';
 import Graticule from 'ol/layer/Graticule';
 import TileLayer from 'ol/layer/Tile';
 import {TileDebug, XYZ} from 'ol/source';
 import {get as getProjection, getTransform, transform} from 'ol/proj';
 import {register} from 'ol/proj/proj4';
 import OSM from 'ol/source/OSM';
-import TileImage from 'ol/source/TileImage';
-import Projection from 'ol/proj/Projection';
-import Stroke from 'ol/style/Stroke';
 import proj4 from 'proj4';
+
+var p3111 = getProjection('3111')
 
 var map = new Map({
   layers: [
@@ -19,33 +18,48 @@ var map = new Map({
       source: new OSM()
     }),
     new TileLayer({
-        source: new TileDebug()
-      }), 
+      source: new XYZ({
+          url: 'https://base.maps.vic.gov.au/wmts/AERIAL_VG/EPSG:3111/{z}/{x}/{y}.png', 
+          proj: p3111
+      })
+    }),
+    new TileLayer({
+      source: new TileDebug()
+    }), 
   ],
   target: 'map',
   view: new View({
-    projection: 'EPSG:3857',
+    projection: 'EPSG:4326',
     center: [0, 0],
     zoom: 1
   })
 });
 
-var queryInput = document.getElementById('epsg-query');
-var searchButton = document.getElementById('epsg-search');
-var resultSpan = document.getElementById('epsg-result');
+
+var code = 3111
+var bbox = []
+fetch(`https://epsg.io/?format=json&q=${code}`).then(function(response) {
+  return response.json();
+}).then(function(json) {
+  var results = json['results'];
+  for (var i = 0, ii = results.length; i < ii; i++) {
+    var result = results[i];
+    if (result) {
+      code = result['code'];
+      console.log(result)
+      var name = result['name'];
+      var proj4def = result['proj4'];
+      bbox = result['bbox'];
+      if (code && code.length > 0 && proj4def && proj4def.length > 0 &&
+          bbox && bbox.length == 4) {
+        setProjection(code, name, proj4def, bbox);
+        return;
+      }
+    }
+  }
+});
 
 function setProjection(code, name, proj4def, bbox) {
-  if (code === null || name === null || proj4def === null || bbox === null) {
-    resultSpan.innerHTML = 'Nothing usable found, using EPSG:3857...';
-    map.setView(new View({
-      projection: 'EPSG:3857',
-      center: [0, 0],
-      zoom: 1
-    }));
-    return;
-  }
-
-  resultSpan.innerHTML = '(' + code + ') ' + name;
 
   var newProjCode = 'EPSG:' + code;
   proj4.defs(newProjCode, proj4def);
@@ -69,45 +83,3 @@ function setProjection(code, name, proj4def, bbox) {
   map.setView(newView);
   newView.fit(extent);
 }
-
-
-function search(query) {
-  resultSpan.innerHTML = 'Searching ...';
-  fetch('https://epsg.io/?format=json&q=' + query).then(function(response) {
-    return response.json();
-  }).then(function(json) {
-    var results = json['results'];
-    if (results && results.length > 0) {
-      for (var i = 0, ii = results.length; i < ii; i++) {
-        var result = results[i];
-        if (result) {
-          var code = result['code'];
-          var name = result['name'];
-          var proj4def = result['proj4'];
-          var bbox = result['bbox'];
-          if (code && code.length > 0 && proj4def && proj4def.length > 0 &&
-              bbox && bbox.length == 4) {
-            setProjection(code, name, proj4def, bbox);
-            return;
-          }
-        }
-      }
-    }
-    setProjection(null, null, null, null);
-  });
-}
-
-
-/**
- * Handle click event.
- * @param {Event} event The event.
- */
-searchButton.onclick = function(event) {
-  search(queryInput.value);
-  event.preventDefault();
-};
-
-
-/**
- * Handle checkbox change event.
- */
